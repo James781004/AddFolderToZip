85d004f8ca9ecf7ceac5cb02b51cea2affa4c7bd
James781004@hotmail.com <James781004@hotmail.com>
2017-12-07 12:15:47 +0800

    V3.0

--- Desktop/backups/AddListToZip.java
+++ Desktop/backups/AddListToZip.java
@@ -1,5 +1,8 @@
 package tw.gov.nca.gss.common.util;
 
+import java.io.File;
+import java.util.ArrayList;
+
 import net.lingala.zip4j.core.ZipFile;
 import net.lingala.zip4j.exception.ZipException;
 import net.lingala.zip4j.model.ZipParameters;
@@ -7,19 +10,11 @@ import net.lingala.zip4j.util.Zip4jConstants;
 import tw.gov.nca.gss.common.GssBusinessException;
 import tw.gov.nca.gss.common.GssMessage;
 
-public class AddFolderToZip {
+public class AddListToZip{
 
-	public static void main(String[] args) {
-		try {
-			new AddFolderToZip();
-		} catch (ZipException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-
- // 
-	public AddFolderToZip() throws ZipException {
+	
+	// 測試
+	public AddListToZip() throws ZipException {
 		try {
 			ZipFile zipFile = new ZipFile("C:/Users/JamesHuang/Desktop/try.zip");
 			String folderToAdd = "C:/Users/JamesHuang/Desktop/backups";
@@ -37,19 +32,31 @@ public class AddFolderToZip {
 		}
 	}
 
-	public AddFolderToZip(final String path, final String folderToAdd, final String pwd) throws ZipException {
+	public AddListToZip(final String path, final String folderToAdd, final String pwd) throws ZipException {
 		try {
 			ZipFile zipFile = new ZipFile(path);
+			ArrayList<File> list = new ArrayList<File>();
+			list.add(new File("file1"));
+			list.add(new File("file2"));
+			list.add(new File("file3"));
+
 			ZipParameters parameters = new ZipParameters();
+
+			// 壓縮格式
 			parameters.setCompressionMethod(Zip4jConstants.COMP_DEFLATE);
+			// 壓縮等級
 			parameters.setCompressionLevel(Zip4jConstants.DEFLATE_LEVEL_NORMAL);
 
+			// 加密以及加密格式
 			parameters.setEncryptFiles(true);
 			parameters.setEncryptionMethod(Zip4jConstants.ENC_METHOD_STANDARD);
 			parameters.setPassword(pwd);
-			zipFile.addFolder(folderToAdd, parameters);
+
+			// zip檔案分割壓縮1MB
+			zipFile.createZipFile(list, parameters, true, 1048576);
 		} catch (Exception e) {
 			throw new GssBusinessException(GssMessage.G000AE, e);
 		}
 	}
+
 }
